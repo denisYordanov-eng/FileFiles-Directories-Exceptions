0f8f884a54a161a77f80ac79fdc64703a05a2179
denisYordanov-eng <denisivov437@gmail.com>
2025-05-15 18:43:41 +0300

    Made a task.

---
+++ 01-Odd Lines/01-OddLines/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01_OddLines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("input.txt", "Two households, both alike in dignity,\r\n" +
+                "In fair Verona, where we lay our scene,\r\n" +
+                "From ancient grudge break to new mutiny,\r\n" +
+                "Where civil blood makes civil hands unclean.\r\n" +
+                "From forth the fatal loins of these two foes\r\n" +
+                "A pair of star-cross'd lovers take their life;\r\n" +
+                "Whose misadventured piteous overthrows\r\n" +
+                "Do with their death bury their parents' strife.\r\n");
+
+           var lines = File.ReadAllLines("input.txt");
+            var oddLines =
+                lines.Where((line,index) => index % 2 != 0);
+            File.WriteAllLines("oddLines.txt", oddLines);
+        }
+    }
+}
