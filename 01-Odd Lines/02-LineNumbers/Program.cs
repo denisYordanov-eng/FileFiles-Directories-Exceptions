6f15d00acbf194696cf517484345802fd582e9d8
denisYordanov-eng <denisivov437@gmail.com>
2025-05-17 07:31:41 +0300

    Made a task.

---
+++ 01-Odd Lines/02-LineNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LineNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("input.txt",
+                "Two households, both alike in dignity,\r\n" +
+                "In fair Verona, where we lay our scene,\r\n" +
+                "From ancient grudge break to new mutiny,\r\n" +
+                "Where civil blood makes civil hands unclean.\r\n" +
+                "From forth the fatal loins of these two foes\r\n" +
+                "A pair of star-cross'd lovers take their life;\r\n" +
+                "Whose misadventured piteous overthrows\r\n" +
+                "Do with their death bury their parents' strife.\r\n");
+
+            string[] lines = File.ReadAllLines("input.txt");
+            var numberedLines = lines.Select ((line, index) => $"{index + 1}.{line}");
+            File.WriteAllLines("output.txt", numberedLines);
+        }
+    }
+}
