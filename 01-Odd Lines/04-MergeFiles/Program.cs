0c0ec37d2bcaf05d881cbac7eab7bcd72d014925
denisYordanov-eng <denisivov437@gmail.com>
2025-05-18 06:50:13 +0300

    Made a task.

---
+++ 01-Odd Lines/04-MergeFiles/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _04_MergeFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("input1.txt",
+                "1\r\n" +
+                "3\r\n" +
+                "5\r\n");
+            File.WriteAllText("input2.txt", 
+                "2\r\n" +
+                "4\r\n" +
+                "6\r\n");
+
+            string[] firstFile = File.ReadAllLines("input1.txt");
+            string[] secondFile = File.ReadAllLines("input2.txt");
+
+            int maxLines = Math.Max(firstFile.Length, secondFile.Length);
+            using (StreamWriter writer = new StreamWriter("output.txt")) 
+            for (int i = 0; i < maxLines; i++)
+            {
+                if(i < firstFile.Length)
+                {
+                    writer.WriteLine(firstFile[i]);
+                }
+                if(i < secondFile.Length)
+                    {
+                        writer.WriteLine(secondFile[i]);
+                    }
+            }
+        }
+    }
+}
