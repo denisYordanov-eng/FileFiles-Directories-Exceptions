e11009af4602e389bcb178311d28bfe48f47181b
denisYordanov-eng <denisivov437@gmail.com>
2025-05-19 13:18:23 +0300

    Get my directory from my c# projects.

---
+++ 01-Odd Lines/GetAllDirectoryFromMyC#Projects/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace GetAllDirectoryFromC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListFile(@"C:\Users\Laptop\Desktop\C#");
+        }
+
+        private static void ListFile(string path)
+        {
+            var getFiles = Directory.GetFiles(path);
+
+            foreach (var file in getFiles)
+            {
+                Console.WriteLine(file);
+            }
+            var subdirs = Directory.GetDirectories(path);
+            foreach (var d in subdirs)
+            {
+                ListFile(d);
+            }
+        }
+    }
+}
