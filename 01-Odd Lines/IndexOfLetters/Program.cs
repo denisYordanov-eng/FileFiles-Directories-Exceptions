a99e8f417117a0856b415ad14b4cd40a1eb000d6
denisYordanov-eng <denisivov437@gmail.com>
2025-05-19 16:33:29 +0300

    made an old task using input.txt as input and output.txt as a output and I used a method File.AppendAllText to append on every iteration of for loop.

---
+++ 01-Odd Lines/IndexOfLetters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace IndexOfLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("input.txt", "softuni");
+            var inputWord = File.ReadAllText("input.txt");
+           
+            for (int i = 0; i < inputWord.Length; i++)
+            {
+                char letter = inputWord[i];
+
+                int index = Math.Abs(letter - 97);
+                File.AppendAllText("output.txt", $"{letter} -> {index}\r\n");
+            }
+
+        }
+    }
+}
