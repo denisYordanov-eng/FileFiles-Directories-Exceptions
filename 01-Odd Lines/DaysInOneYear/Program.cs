7be24d4b92127c5c645a39189619de6218cb4041
denisYordanov-eng <denisivov437@gmail.com>
2025-05-19 07:25:22 +0300

    Made a task to understand using \r and \n.

---
+++ 01-Odd Lines/DaysInOneYear/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DaysInOneYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 365; i++)
+            {
+                
+                Console.Write("\r");
+                Console.Write("days in a year: " + i);
+                   
+            }
+        }
+    }
+}
